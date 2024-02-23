98481fae27244e58e3f65c7ee37c532c2ff1ae24
Priyanka Pardeshi <priyanka.pardeshi@sysforetech.local>
2024-02-23 11:26:16 +0530

    Created blazor web app and blazor hybrid app

---
+++ ByteTOImag/ByteTOImag/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System;
+using System.Drawing;
+using System.IO;
+
+Console.WriteLine("Hello, World!");
+
+string userInput = Console.ReadLine(); // Read user input from console
+
+ ImageToByteArrayFromFilePath(userInput);
+byte[] ImageToByteArrayFromFilePath(string imagefilePath)
+{
+    byte[] imageArray = File.ReadAllBytes(imagefilePath);
+    File.WriteAllBytes("C:\\Users\\priyanka.pardeshi\\Desktop\\generateimg.jpg", imageArray);
+    return imageArray;
+}
